17808d23b576afe6e7c32208e7c718d8d57b11da
Ominox0 <franstanic24@gmail.com>
2024-07-12 22:26:22 +0200

    .

--- src/main/java/com/folumo/mekanism_lasers/Mekanism_lasers.java
+++ src/main/java/com/folumo/mekanism_lasers/Mekanism_lasers.java
@@ -1,6 +1,5 @@
 package com.folumo.mekanism_lasers;
 
-import com.folumo.mekanism_lasers.client.ClientSetup;
 import com.folumo.mekanism_lasers.common.registry.*;
 import com.mojang.logging.LogUtils;
 import net.neoforged.fml.common.Mod;
@@ -32,11 +31,11 @@ public class Mekanism_lasers {
 
 /* TODO:
  *  new features/modification in next update:
- *  1. New laser type (Toggleable Laser) -> queened
+ *  1. New laser type (Toggleable Laser) -> in progress
  *  2. Remote Control to turn on/off Toggleable Lasers -> queened
  *  3. Remote Interface (you have to put Remove Control in it to work) -> queened
  *      -Takes redstone signal and based on signal it will turn on/off Toggleable Lasers
- *  4. Make Ore Generator be able to rotate -> in progress
+ *  4. Make Ore Generator be able to rotate -> Done
  *  5. Laser Configurator -> queened
  *      -Make it so that you can open gui for any laser except mekanism:laser
  *      -Empty gui with only title
