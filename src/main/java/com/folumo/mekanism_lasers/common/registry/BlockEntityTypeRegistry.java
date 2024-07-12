a91af51047cfb3b42bec907f03d85237e9969221
Ominox0 <franstanic24@gmail.com>
2024-07-12 20:25:42 +0200

    .

--- src/main/java/com/folumo/mekanism_lasers/common/registry/BlockEntityTypeRegistry.java
+++ src/main/java/com/folumo/mekanism_lasers/common/registry/BlockEntityTypeRegistry.java
@@ -7,28 +7,48 @@ import com.folumo.mekanism_lasers.common.block_entity.laser.AdvancedLaserBlockEn
 import com.folumo.mekanism_lasers.common.block_entity.laser.BasicLaserBlockEntity;
 import com.folumo.mekanism_lasers.common.block_entity.laser.EliteLaserBlockEntity;
 import com.folumo.mekanism_lasers.common.block_entity.laser.UltimateLaserBlockEntity;
+import mekanism.common.capabilities.Capabilities;
 import mekanism.common.registration.impl.TileEntityTypeDeferredRegister;
 import mekanism.common.registration.impl.TileEntityTypeRegistryObject;
+import mekanism.common.tile.base.TileEntityMekanism;
 
 public class BlockEntityTypeRegistry {
 
     public static final TileEntityTypeDeferredRegister BLOCK_ENTITY_TYPES = new TileEntityTypeDeferredRegister(Mekanism_lasers.MOD_ID);
 
     public static final TileEntityTypeRegistryObject<LaserStopperBlockEntity> LASER_STOPPER =
-            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.LASER_STOPPER, LaserStopperBlockEntity::new).build();
+            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.LASER_STOPPER, LaserStopperBlockEntity::new)
+                    .serverTicker(TileEntityMekanism::tickServer)
+                    .withSimple(Capabilities.LASER_RECEPTOR)
+                    .build();
 
     public static final TileEntityTypeRegistryObject<BasicLaserBlockEntity> BASIC_LASER =
-            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.BASIC_LASER, BasicLaserBlockEntity::new).build();
+            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.BASIC_LASER, BasicLaserBlockEntity::new)
+                    .serverTicker(TileEntityMekanism::tickServer)
+                    .withSimple(Capabilities.LASER_RECEPTOR)
+                    .build();
 
     public static final TileEntityTypeRegistryObject<AdvancedLaserBlockEntity> ADVANCED_LASER =
-            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.ADVANCED_LASER, AdvancedLaserBlockEntity::new).build();
+            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.ADVANCED_LASER, AdvancedLaserBlockEntity::new)
+                    .serverTicker(TileEntityMekanism::tickServer)
+                    .withSimple(Capabilities.LASER_RECEPTOR)
+                    .build();
 
     public static final TileEntityTypeRegistryObject<EliteLaserBlockEntity> ELITE_LASER =
-            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.ELITE_LASER, EliteLaserBlockEntity::new).build();
+            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.ELITE_LASER, EliteLaserBlockEntity::new)
+                    .serverTicker(TileEntityMekanism::tickServer)
+                    .withSimple(Capabilities.LASER_RECEPTOR)
+                    .build();
 
     public static final TileEntityTypeRegistryObject<UltimateLaserBlockEntity> ULTIMATE_LASER =
-            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.ULTIMATE_LASER, UltimateLaserBlockEntity::new).build();
+            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.ULTIMATE_LASER, UltimateLaserBlockEntity::new)
+                    .serverTicker(TileEntityMekanism::tickServer)
+                    .withSimple(Capabilities.LASER_RECEPTOR)
+                    .build();
 
     public static final TileEntityTypeRegistryObject<OreGeneratorBlockEntity> ORE_GENERATOR =
-            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.ORE_GENERATOR, OreGeneratorBlockEntity::new).build();
+            BLOCK_ENTITY_TYPES.mekBuilder(BlockRegistry.ORE_GENERATOR, OreGeneratorBlockEntity::new)
+                    .serverTicker(TileEntityMekanism::tickServer)
+                    .withSimple(Capabilities.LASER_RECEPTOR)
+                    .build();
 }
