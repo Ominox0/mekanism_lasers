75bd1e249e64b52d5e1ffe11b5124260790cd152
Ominox0 <franstanic24@gmail.com>
2024-07-12 20:51:17 +0200

    .

--- src/main/java/com/folumo/mekanism_lasers/common/block_entity/OreGeneratorBlockEntity.java
+++ src/main/java/com/folumo/mekanism_lasers/common/block_entity/OreGeneratorBlockEntity.java
@@ -16,25 +16,26 @@ import mekanism.common.inventory.slot.OutputInventorySlot;
 import mekanism.common.tile.laser.TileEntityLaserReceptor;
 import mekanism.common.util.InventoryUtils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.Tags;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Random;
+
 
 public class OreGeneratorBlockEntity extends TileEntityLaserReceptor {
     private int coolDown = 0;
     public static FloatingLong energyCap = FloatingLong.createConst(64_000_000L);
     public static FloatingLong usage = FloatingLong.createConst(16_000_000L);
     private LaserEnergyContainer energyContainer;
+
 
     @NotNull
     @Override
@@ -107,17 +108,9 @@ public class OreGeneratorBlockEntity extends TileEntityLaserReceptor {
     }
 
     private static ItemStack getRandomDrop() {
-        TagKey<Block> blockTag = BlockTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ore"));
-        List<HolderSet.Named<Block>> blocksInTag = BuiltInRegistries.BLOCK.getTag(blockTag).stream().toList();
-
-        Random rand = new Random();
-
-        // Get a random index
-        int randomIndex = rand.nextInt(blocksInTag.size());
-
-
-        HolderSet.Named<Block> block = blocksInTag.get(randomIndex);
-
-        return new ItemStack(block.get(0).value().asItem());
+        HolderSet.Named<Block> oreTag = BuiltInRegistries.BLOCK.getTag(Tags.Blocks.ORES).orElseThrow();
+        Holder<Block> randomBlockHolder = oreTag.getRandomElement(RandomSource.create()).orElseThrow();
+        Block randomBlock = randomBlockHolder.value();
+        return new ItemStack(randomBlock.asItem());
     }
 }
