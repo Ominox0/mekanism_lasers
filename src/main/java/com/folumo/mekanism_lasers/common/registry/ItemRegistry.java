e0445aa8b341302a5acd2a472434083a8200be21
Ominox0 <franstanic24@gmail.com>
2024-07-12 20:02:52 +0200

    .

--- src/main/java/com/folumo/mekanism_lasers/common/registry/ItemRegistry.java
+++ src/main/java/com/folumo/mekanism_lasers/common/registry/ItemRegistry.java
@@ -1,28 +1,37 @@
 package com.folumo.mekanism_lasers.common.registry;
 
 import com.folumo.mekanism_lasers.Mekanism_lasers;
+import mekanism.common.block.BlockBounding;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ItemRegistry {
-
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Mekanism_lasers.MOD_ID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Mekanism_lasers.MOD_ID);
 
     public static class ModItemTab {
 
         public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Mekanism_lasers.MOD_ID);
 
-        public static final RegistryObject<CreativeModeTab> MEKT_ITEM_TAB = CREATIVE_MODE_TABS.register("main", () -> CreativeModeTab.builder()
+        public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MEKANISM_LASERS_ITEM_TAB = CREATIVE_MODE_TABS.register("main", () -> CreativeModeTab.builder()
                 .icon(() -> BlockRegistry.ULTIMATE_LASER.getItemStack().getItem().getDefaultInstance())
                 .title(Component.translatable("item_group." + Mekanism_lasers.MOD_ID))
                 .displayItems((displayParameters, output) -> {
                     ItemRegistry.ITEMS.getEntries().forEach(itemRegistryObject -> output.accept(itemRegistryObject.get()));
-                    BlockRegistry.BLOCKS.getAllBlocks().forEach(iBlockProvider -> output.accept(iBlockProvider.getItemStack()));
+
+                    for (Holder<Block> blockHolder : BlockRegistry.BLOCKS.getPrimaryEntries()) {
+                        Block block = blockHolder.value();
+                        // Don't add bounding blocks to the creative tab
+                        if (!(block instanceof BlockBounding)) {
+                            output.accept(new ItemStack(block));
+                        }
+                    }
+
                 })
                 .build());
     }
